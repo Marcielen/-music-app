3756a34c5703a45e1535ca5b3ce51fe0df78887d
marcielen barbosa <marcielenmmb@hotmail.com>
2023-01-27 11:06:26 -0300

    feat: ajust code

--- src/components/PlayerMusic/ControlMusic/index.tsx
+++ src/components/PlayerMusic/ControlMusic/index.tsx
@@ -17,7 +17,7 @@ import {
 } from "react-icons/ai";
 import { MdGraphicEq } from "react-icons/md";
 
-import { convertDurationToTimeString } from "Format/FormatTimeDurationMusic";
+import { convertDurationToTimeString } from "Format/formatTimeMusic";
 import { useMusicContext } from "store/contextMusic";
 
 import { AleatoryMusicIcon, RepeatMusicIcon } from "icons";
