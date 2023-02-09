28507805758a7ef544265d35af1400e5d9a8ecf6
Marcielen Barbosa <marcielenmmb@hotmail.com>
2023-02-09 19:16:11 -0300

    feat: test mobile

--- src/pages/genere/[id]/index.tsx
+++ src/pages/genere/[id]/index.tsx
@@ -29,7 +29,22 @@ export default function Genere() {
   });
 
   return (
-    <Box h={mobile ? "85vh" : undefined} color="white" w="full">
+    <Box
+      sx={{
+        "&::-webkit-scrollbar": {
+          height: "0",
+          width: "0",
+        },
+        "& .virtualized_List::-webkit-scrollbar": {
+          height: "0",
+          width: "0",
+        },
+      }}
+      overflow="auto"
+      h={mobile ? "85vh" : undefined}
+      color="white"
+      w="full"
+    >
       <Box
         pb="25px"
         bgGradient="linear-gradient(to left, #ce79ca, #b06fba, #9464a9, #7a5996, #624e83, #57426f, #4b375b, #3f2d49, #352234, #271922, #190f14, #000000);"
